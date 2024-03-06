1977de2af11ba342f87c4ef583422fd0d09b0c0f
prakash2104 <prakashkumar68553495@gmail.com>
2024-03-06 10:37:48 +0530

    few things added

--- 2_first_vite_project/src/main.jsx
+++ 2_first_vite_project/src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-function MyApp() {
-  return (
-    <div>
-      <h1>Hiii there</h1>
-    </div>
-  );
-}
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Hiii there</h1>
+//     </div>
+//   );
+// }
 // const ReactElement = {
 //   type: "a",
 //   props: {
